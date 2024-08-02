f705d73743a038cfab08b10eb6d7ef6ac0cfe00e
devanshdeveloper <devanshkhetwani@gmail.com>
2024-08-02 20:21:38 +0530

    may be fixed

--- utils/s3-bucket.js
+++ utils/s3-bucket.js
@@ -7,6 +7,10 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export const s3Client = new S3Client({
   region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 export async function getObjectURL({ Bucket, Key }) {
